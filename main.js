57c61218b57153b68e5151e278311d083758dcdf
Mico Irvin <mico@micoirvin.com>
2022-06-22 15:52:38 +0800

    Completed the features specified by FrontendMentor

--- main.js
+++ main.js
@@ -1,140 +1,277 @@
+let data = JSON.parse(localStorage.getItem(`data-intrctv-cmmnts-by-mico-irvin`));
+
+const updateLocalStorage = function() {
+   localStorage.setItem(`data-intrctv-cmmnts-by-mico-irvin`, JSON.stringify(data));
+   console.log(`Local storage updated.`)
+}
+
 const createPost = function(btn) {
    let post = btn.parentElement;
-   let content = post.querySelector(`textarea`).value;
-   console.log(content);
+   let textarea = post.querySelector(`textarea`);
+   let content = textarea.value;
+   textarea.value = ``;
+   let parentId = post.parentElement.id;
+
+   if (parentId[0] !== `p`) {
+      post.remove(); // Comment form does not disappear. Reply form disappears.
+   }
 
    if (content === `` || typeof content === `undefined` || content === null) {
       return;
    }
-   let key;
+
+   let postId = `p`;
    if (post.classList.contains(`reply`)) {
-      key = `replies`;
+      postId = `r`;
    }
    else if (post.classList.contains(`cmmnt`)) {
-      key = `comments`;
+      postId = `c`;
    }
 
-   let form = post.parentElement;
-   let postBox = form.parentElement;
-   let postSection = postBox.querySelector(`div.${key}`);
+   data[`globalCurrentId`] += 1;
+   postId += data[`globalCurrentId`];
 
+   data[`posts`][postId] = {
+      "parentId": parentId,
+      "content": content,
+      "createdAt": "Just now",
+      "score": 0,
+      "user": data["currentUser"],
+      "childrenIds": []
+   };
 
+   if (parentId !== `p0`) {
+      data[`posts`][parentId]["childrenIds"].push(postId);
+   }
 
-
-   posts   
-
-   // let postBox = document.createElement(`div.post-box`);
-   postBox.innerHTML =
-      `<div class="post cmmnt">
-         <a href="#" class="avtr"></a>
-         <span class="usrnme-box">
-            <a href="#" class="usrnme">admin</a>
-            <a class="user-role">you</a>
-         </span>
-         <a class="post-date">2 weeks ago</a>
-
-         <p class="post-main">${content}</p>
-
-         <span class="votes">
-            <button class="vote-btn" type="button">+</button>
-            <span class="score">100</span>
-            <button class="vote-btn" type="button">&minus;</button>
-         </span>
-
-         <button class="small-btn del" type="button"><i class="fa-solid fa-trash"></i>Delete</button>
-
-         <button class="small-btn edit" type="button"><i class="fa-solid fa-pen"></i>Edit</button>
-      </div>`;
-
-   // console.log(posts);
-   posts.insertBefore(postBox, form);
-
+   console.log(data);
+   updateLocalStorage();
+   readPost(postId);
 }
 
+const createPostForm = function(id, type = `reply`) {
+   let postBox = document.querySelector(`#${id}`);
+   let form = document.createElement(`div`);
+   postBox.appendChild(form);
+   form.classList.add(`post`, `form`, `create`);
+   let placeholder = `What's on your mind...`;
+   let btnContent = `POST`;
 
+   if (type === `cmmnt`) {
+      form.classList.add(`cmmnt`);
+      placeholder = `Add a comment...`;
+      btnContent = `SEND`;
+   }
+   else if (type === `reply`) {
+      let currentForm = document.querySelector(`.post.form.create.cmmnt.reply`);
+      if (typeof currentForm !== `undefined` && currentForm !== null){
+         currentForm.remove();
+      }
+      form.classList.add(`cmmnt`, `reply`);
+      placeholder = `Send a reply...`;
+      btnContent = `REPLY`;
+   }
 
-let data = JSON.parse(localStorage.getItem(`data-intrctv-cmmnts-by-mico-irvin`));
+   form.innerHTML =  
+   `<a href="#" class="avtr"><img src="${data["users"][data["currentUser"]]["image"]["png"]}"></a>
+   <textarea name="" id="" placeholder="${placeholder}" ></textarea>
+   <button type="submit" name="send-reply" class="big-btn" onclick="createPost(this)">${btnContent}</button>`;
+}
 
-const readPost = function(dataSet, key, i) {
-   let post = dataSet[key][i];
-   let parentId = `p${dataSet["id"]}`;
-   let postId = `p${post["id"]}`;
+const readPost = function(id) {
+   let post = document.querySelector(`#${id}`);
+   if (typeof post !== `undefined` && post !== null) {
+      return; // The post was already read.
+   }
+   post = data[`posts`][id];
+   let parentId = post[`parentId`];
+   // console.log(
+   //    `parentId = ${parentId}
+   //    id = ${id}
+   //    post = ${post["content"]}`
+   // );
+
    let parentPost = document.querySelector(`#${parentId}`);
-   let postSection = parentPost.querySelector(`div.${key}`);
-
+   if (typeof parentPost === `undefined` || parentPost === null) {
+      // If the parent does not exist yet.
+      readPost(parentId);
+      parentPost = document.querySelector(`#${parentId}`);
+      // Only works if the parent is a comment.
+      // Still need to create a CSS class for a parent that is a post. 
+      // Right now it is main#p0.
+   }
+   
+   let sections = {
+      "p": "posts",
+      "c": "comments",
+      "r": "replies"
+   };
+   let sectionKey = sections[id[0]];
+   let postSection = parentPost.querySelector(`.${sectionKey}`);
    if (typeof postSection === `undefined` || postSection === null) {
       // If comments section or replies section does not exist yet.
       postSection = document.createElement(`div`);
-      postSection.classList.add(`${key}`);
+      postSection.classList.add(sectionKey);
       parentPost.appendChild(postSection);
    }
-   
-   console.log(
-      `parentId = ${parentId}
-      postId = ${postId}
-      post = ${post["content"]}`
-   );
-
-   let cssClass;
-   if (key === `comments`) cssClass = `cmmnt`;
-   else if (key === `replies`) cssClass = `cmmnt reply`;
 
    let postBox = document.createElement(`div`);
    postBox.classList.add(`post-box`);
-   postBox.setAttribute(`id`, postId);
+   postBox.setAttribute(`id`, id);
    postSection.appendChild(postBox);
+   
+   let cssClass = ``;
+   if (sectionKey === `comments`) cssClass = `cmmnt`;
+   else if (sectionKey === `replies`) cssClass = `cmmnt reply`;
+
+   let userRole = ``;
+   let smallBtns = 
+   `<button class="small-btn reply" type="button" onclick="createPostForm('${id}')"><i class="fa-solid fa-reply"></i>Reply</button>`;
+   if (post[`user`] === data[`currentUser`]){
+      userRole = `<a class="user-role">you</a>`;
+      smallBtns = 
+      `<button class="small-btn del" type="button" onclick="trigDelModal('${id}')"><i class="fa-solid fa-trash"></i>Delete</button>
+      <button class="small-btn edit" type="button" onclick="createEditForm('${id}')"><i class="fa-solid fa-pen"></i>Edit</button>
+      <button type="submit" class="small-btn cncl">Cancel</button>
+      <button type="submit" class="big-btn" onclick="updatePost('${id}')">UPDATE</button>
+      <textarea></textarea>`;
+   }
+   
    postBox.innerHTML =
-      `<div class="post ${cssClass}">
-         <a href="#" class="avtr"></a>
-         <span class="usrnme-box">
-            <a href="#" class="usrnme">${post["user"]["username"]}</a>
-            <a class="user-role">?</a>
-         </span>
-         <a class="post-date">${post["createdAt"]}</a>
+   `<div class="post ${cssClass}">
+   <a href="#" class="avtr"><img src="${data["users"][post["user"]]["image"]["png"]}"></a>
+   <span class="usrnme-box">
+      <a href="#" class="usrnme">${post["user"]}</a>
+      ${userRole}
+   </span>
+   <a class="post-date">${post["createdAt"]}</a>
 
-         <p class="post-main">${post["content"]}</p>
+   <p class="post-main">${post["content"]}</p>
+   
+   <span class="votes">
+      <button class="vote-btn" type="button" onclick="addScore('${id}', 1)">+</button>
+      <span class="score">${post["score"]}</span>
+      <button class="vote-btn" type="button" onclick="addScore('${id}', -1)">&minus;</button>
+   </span>
 
-         <span class="votes">
-            <button class="vote-btn" type="button">+</button>
-            <span class="score">${post["score"]}</span>
-            <button class="vote-btn" type="button">&minus;</button>
-         </span>
-
-         <button class="small-btn del" type="button"><i class="fa-solid fa-trash"></i>Delete</button>
-
-         <button class="small-btn edit" type="button"><i class="fa-solid fa-pen"></i>Edit</button>
-      </div>
-      `;
+   ${smallBtns}
+   </div>
+      `;   
 }
 
-const loopPosts = function(dataSet, key) {
-   for(let i in dataSet[key]) {
-      readPost(dataSet, key, i);
-      let replies = dataSet[key][i][`replies`];
-      if (typeof replies !== `undefined` && replies !== null && replies.length > 0) {
-         // Loop through replies if there are replies.
-         loopPosts(dataSet[key][i], `replies`);
-      }
+const updatePost = function(id) {
+   let post = document.querySelector(`#${id} > .post.form`);
+   let content = post.querySelector(`textarea`).value;
+
+   data[`posts`][id][`content`] = content;
+   console.log(data);
+   updateLocalStorage();
+   post.querySelector(`.post-main`).innerHTML = content;
+
+   closeEditForm();
+}
+
+const closeEditForm = function() {
+   let currentForm = document.querySelector(`.post.form.updt`);
+   if (typeof currentForm !== `undefined` && currentForm !== null){
+      currentForm.classList.remove(`form`, `updt`);
    }
+
+}
+
+const createEditForm = function(id) {
+   closeEditForm();
+   let post = document.querySelector(`#${id}`).querySelector(`.post`);
+   post.classList.add(`form`, `updt`);
+   let content = post.querySelector(`.post-main`).innerHTML;
+
+   let textarea = post.querySelector(`textarea`);
+   textarea.value = content;
+}
+
+const trigDelModal = function(id) {
+   let modalBg = document.querySelector(`#del-modal`);
+   modalBg.style.display = `flex`;
+   
+   let modalDelBtn = modalBg.querySelector(`.big-btn.yes`);
+   modalDelBtn.setAttribute(`onclick`, `deletePost('${id}')`);
+
+}
+
+const closeDelModal = function() {
+   let modalBg = document.querySelector(`#del-modal`);
+   modalBg.style.display = `none`;
+}
+
+const deleteFamily = function(id) {
+   for(let i = data[`posts`][id][`childrenIds`].length; i > 0; i--) {
+      let childId = data[`posts`][id][`childrenIds`].pop();
+      deleteFamily(childId);
+   }
+   delete data[`posts`][id];
+   console.log(`deleted ${id}`);
+   console.log(data[`posts`]);
+}
+
+const deletePost = function(id) {
+   let postBox = document.querySelector(`#${id}`);
+   deleteFamily(id);
+   postBox.remove();
+   updateLocalStorage();
+   closeDelModal();
+}
+
+const addScore = function(id, inc) {
+   data[`posts`][id][`score`] += inc;
+   updateLocalStorage();
+   let score = document.querySelector(`#${id} > .post span.score`);
+   score.innerHTML = data[`posts`][id][`score`];
+}
+
+const changeUser = function() {
+   let newUser = document.querySelector(`#chng-user`).value;
+   if (data[`currentUser`] !==  newUser) {
+      data[`currentUser`] = newUser;
+      updateLocalStorage();
+      clearElement(`main`);
+      programFlow();
+      // console.log(newUser);
+   }
+
+}
+
+const clearElement = function(e) {
+   element = document.querySelector(e);
+   while(element.firstChild && element.removeChild(element.firstChild));
 }
 
 const programFlow = function() {
-   console.log(`Nice flow.`);
-   loopPosts(data, `comments`);
-   let createBtns = document.querySelectorAll(`.big-btn.create`);
-   // const test = document.getElementById(`test`);
-   createBtns.forEach(function(btn) {
-      btn.addEventListener(`click`, function() {
-         createPost(btn);
-      });
-   });
+   // console.log(`Nice flow.`);
+   for(let id in data[`posts`]) {
+      readPost(id);
+   }
+
+   let selectUser = document.querySelector(`#chng-user`);
+   while(selectUser.firstChild && selectUser.removeChild(selectUser.firstChild));
+
+   for(let user in data[`users`]) {
+      let option = document.createElement(`option`);
+      option.value = user;
+      option.innerHTML = user;
+      selectUser.appendChild(option);
+      if(user === data[`currentUser`]) {
+         option.setAttribute(`selected`, true);
+      }
+   }
+
+   createPostForm("p0", "cmmnt");
 }
 
 const fetchData = function() {
    const initialize = function(fetchedData) {
-      console.log(fetchedData);
-      localStorage.setItem(`data-intrctv-cmmnts-by-mico-irvin`, JSON.stringify(fetchedData));
-      data = JSON.parse(localStorage.getItem(`data-intrctv-cmmnts-by-mico-irvin`));
+      data = fetchedData;
+      console.log(data);
+      updateLocalStorage();
       programFlow();
    }
 
@@ -155,103 +292,23 @@ const fetchData = function() {
    });
 }
 
-
-if (typeof data !== `undefined` && data !== null) {
-   // Let's go!
-   programFlow();
-}
-else {
-   // Data does not exist in the localStorage.
-   fetchData();
+const clearData = function() {
+   localStorage.removeItem(`data-intrctv-cmmnts-by-mico-irvin`);
+   for (let k in data) delete data[k];
+   data = undefined;
+   clearElement(`main`);
 }
 
+const refresh = function(){
+   if (typeof data !== `undefined` && data !== null) {
+      // Let's go!
+      programFlow();
+   }
+   else {
+      // Data does not exist in the localStorage.
+      fetchData();
+   }
 
+}
 
-
-
-
-
-
-
-
-
-
-
-
-
-// try {
-//    data = JSON.parse(localStorage.getItem("data-intrctv-cmmnts-by-mico-irvin"));
-//    console.log(data);
-// }
-// catch {
-//    alert(`hello`)
-// }
-
-
-// localStorage.setItem("data-intrctv-cmmnts-by-mico-irvin", JSON.stringify(data));
-
-
-// let data = JSON.parse(localStorage.getItem("data-intrctv-cmmnts-by-mico-irvin"));
-// console.log(data);
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// const cmmntSection = document.querySelector(`section.comments`);
-// cmmntSection.innerHTML += `
-// <div class="post-box">
-//   <div class="post form create cmmnt">
-//     <a href="#" class="avtr"></a>
-//     <textarea name="" id="" placeholder="Add a comment..." ></textarea>
-//     <button type="submit" name="send-cmmnt" class="big-btn">SEND</button>
-//   </div>
-// </div>`;
-
-
-// let b = document.querySelectorAll(`button[name="send-cmmnt"]`);
-// console.log(b);
-
-// let jsondata;
-
-// const getjsondata = function(x) {
-//    return x;
-// }
-
-// const myfunc = function() {
-//    let a = 0;
-//    fetch("./data.json")
-//    .then(response => {
-//       return response.json();
-//    })
-//    .then(x => {
-//       console.log(x);
-//       a = getjsondata(x);
-//    });
-//    return a;
-// }
-
-// let mico = myfunc();
-
-// console.log(mico);
-
-// let data = JSON.parse(localStorage.getItem("data")) || [];
-// console.log(data);
-
-// let data1 = [];
-
-// data1.push({number: 3.14159});
-// console.log(data1);
-
-// localStorage.setItem("data1", JSON.stringify(data1));
-
-
+refresh();
